f950e519ae257bac6bfadcd73cea2c703ce5921f
Savva <sevasteenko.savva@mail.ru>
2022-05-05 22:57:53 +0300

    Second step: Условия и циклы

--- main.js
+++ main.js
@@ -9,18 +9,23 @@ let personalMovieBD = {
     privat: false
 };
 
+for (let i = 0; i < 3; i++) {
+    let movieLast = prompt("Один из последних просмотренных фильмов?"),
+        movieLastMark = prompt("На сколько его оцениваете?");
+        if (movieLast != null && movieLastMark != null && // Для подстраховки (Нельзя иметь null и '' одновременно))
+            movieLast != "" && movieLastMark != "" && movieLast.length < 50){
+            personalMovieBD.movies[movieLast] = movieLastMark;
+        } else { 
+            i--;
+        } 
+}
 
-
-
-
-const movieLast = prompt("Один из последних просмотренных фильмов?");
-const movieLastMark = prompt("На сколько его оцениваете?");
-personalMovieBD.movies[movieLast] = movieLastMark;
 console.log(personalMovieBD);
-/*
-let movie = {
-    movieLast: movieLastMark,
-};
 
-console.log(movie);
-*/
+if (personalMovieBD.count < 10){
+    console.log("Просмотрено мало фильмов");
+} else if(personalMovieBD.count >= 10 && personalMovieBD.count < 30){
+    console.log("Вы классический зритель");
+} else{
+    console.log("Вы Киноман");
+}
