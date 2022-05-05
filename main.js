01e374914940b2ea475de639b09d1ba3af895f83
Savva <sevasteenko.savva@mail.ru>
2022-05-06 00:40:56 +0300

    Step 3 Part 1 Функции

--- main.js
+++ main.js
@@ -1,5 +1,21 @@
+let numberOfFilms;
 
-const numberOfFilms = +prompt("Сколько фильмов ты посмотрел?");
+function start() {
+    numberOfFilms = +prompt("Сколько фильмов вы посмотрели?");
+
+    while (numberOfFilms == "" || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt("Сколько фильмов вы посмотрели?");
+    }
+}
+/*
+Преобразует в true тодько NaN и string(лучше смотреть документацию, очень много исключений)
+
+Пример почему использование isNaN не всегда уместно
+isNaN("blabla")    true: "blabla" преобразовано в число.
+                   При парсинге преобразуется в число при неудаче 
+*/
+
+start();
 
 let personalMovieBD = {
     count: numberOfFilms,
@@ -9,23 +25,27 @@ let personalMovieBD = {
     privat: false
 };
 
-for (let i = 0; i < 3; i++) {
-    let movieLast = prompt("Один из последних просмотренных фильмов?"),
-        movieLastMark = prompt("На сколько его оцениваете?");
-        if (movieLast != null && movieLastMark != null && // Для подстраховки (Нельзя иметь null и '' одновременно))
-            movieLast != "" && movieLastMark != "" && movieLast.length < 50){
+function rememberMyFilms() {
+    for (let i = 0; i < 3; i++) {
+        let movieLast = prompt("Один из последних просмотренных фильмов?"),
+            movieLastMark = prompt("На сколько его оцениваете?");
+        if (movieLast != null && movieLastMark != null && // null - это отмена ввода '' - пустой Ок)
+            movieLast != "" && movieLastMark != "" && movieLast.length < 50) {
             personalMovieBD.movies[movieLast] = movieLastMark;
-        } else { 
+        } else {
             i--;
-        } 
+        }
+    }
 }
 
 console.log(personalMovieBD);
 
-if (personalMovieBD.count < 10){
-    console.log("Просмотрено мало фильмов");
-} else if(personalMovieBD.count >= 10 && personalMovieBD.count < 30){
-    console.log("Вы классический зритель");
-} else{
-    console.log("Вы Киноман");
+function detectPersonalLevel() {
+    if (personalMovieBD.count < 10) {
+        console.log("Просмотрено мало фильмов");
+    } else if (personalMovieBD.count >= 10 && personalMovieBD.count < 30) {
+        console.log("Вы классический зритель");
+    } else {
+        console.log("Вы Киноман");
+    }
 }
