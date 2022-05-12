7a9b9d4444039f0efa69ec1104c213e3c8db11a9
Savva <sevasteenko.savva@mail.ru>
2022-05-12 18:02:52 +0300

    Step 4 Методы объекта

--- main.js
+++ main.js
@@ -1,13 +1,11 @@
-let numberOfFilms;
-
-function start() {
-    numberOfFilms = +prompt("Сколько фильмов вы посмотрели?");
-
-    while (numberOfFilms == "" || numberOfFilms == null || isNaN(numberOfFilms)) {
-        numberOfFilms = +prompt("Сколько фильмов вы посмотрели?");
-    }
-}
 /*
+function start() {
+        numberOfFilms = +prompt("Сколько фильмов вы посмотрели?");
+    
+        while (numberOfFilms == "" || numberOfFilms == null || isNaN(numberOfFilms)) {
+            numberOfFilms = +prompt("Сколько фильмов вы посмотрели?");
+        }
+    }
 Преобразует в true тодько NaN и string(лучше смотреть документацию, очень много исключений)
 
 Пример почему использование isNaN не всегда уместно
@@ -15,50 +13,62 @@ isNaN("blabla")    true: "blabla" преобразовано в число.
                    При парсинге преобразуется в число при неудаче 
 */
 
-start();
 
 let personalMovieBD = {
-    count: numberOfFilms,
+    count: 0,
     movies: {},
     actors: {},
     genres: [],
-    privat: false
+    privat: false,
+    start: function() {
+        personalMovieBD.count = +prompt("Сколько фильмов вы посмотрели?");
+    
+        while (personalMovieBD.count == "" || personalMovieBD.count == null || isNaN(personalMovieBD.count)) {
+            personalMovieBD.count = +prompt("Сколько фильмов вы посмотрели?");
+        }
+    },
+    rememberMyFilms: function() {
+        for (let i = 0; i < 3; i++) {
+            let movieLast = prompt("Один из последних просмотренных фильмов?"),
+                movieLastMark = prompt("На сколько его оцениваете?");
+            if (movieLast != null && movieLastMark != null && // null - это отмена ввода '' - пустой Ок)
+                movieLast != "" && movieLastMark != "" && movieLast.length < 50) {
+                personalMovieBD.movies[movieLast] = movieLastMark;
+            } else {
+                i--;
+            }
+        }
+    },
+    detectPersonalLevel: function() {
+        if (personalMovieBD.count < 10) {
+            console.log("Просмотрено мало фильмов");
+        } else if (personalMovieBD.count >= 10 && personalMovieBD.count < 30) {
+            console.log("Вы классический зритель");
+        } else {
+            console.log("Вы Киноман");
+        }
+    },
+    writeYourGenres: function() {
+        for (let i = 0; i < 3; i++) {
+            let a = i;
+            while (personalMovieBD.genres== "" || personalMovieBD.genres == null) {
+                personalMovieBD.genres[i] = prompt(`Ваш любимый жанр №${++a}?`);
+            }
+            personalMovieBD.genres.forEach((item, index, array) => {
+                console.log(`${item} имеет позицию ${index} в ${array}`);
+              });
+        }
+    },
+    showMyBd: function() {
+        if (personalMovieBD.privat == false) {
+            console.log(personalMovieBD);
+        }
+    },
+    toggleVisibleMyBD: function(){
+        if (personalMovieBD.privat){
+            personalMovieBD.privat=false;
+        } else{
+            personalMovieBD.privat= true;
+        }
+    }
 };
-
-// function rememberMyFilms() {
-//     for (let i = 0; i < 3; i++) {
-//         let movieLast = prompt("Один из последних просмотренных фильмов?"),
-//             movieLastMark = prompt("На сколько его оцениваете?");
-//         if (movieLast != null && movieLastMark != null && // null - это отмена ввода '' - пустой Ок)
-//             movieLast != "" && movieLastMark != "" && movieLast.length < 50) {
-//             personalMovieBD.movies[movieLast] = movieLastMark;
-//         } else {
-//             i--;
-//         }
-//     }
-// }
-
-// function detectPersonalLevel() {
-//     if (personalMovieBD.count < 10) {
-//         console.log("Просмотрено мало фильмов");
-//     } else if (personalMovieBD.count >= 10 && personalMovieBD.count < 30) {
-//         console.log("Вы классический зритель");
-//     } else {
-//         console.log("Вы Киноман");
-//     }
-// }
-
-
-function writeYourGenres() {
-    for (let i = 0; i < 3; i++) {
-        let a = i;
-        personalMovieBD.genres[i] = prompt(`Ваш любимый жанр №${++a}?`);
-    }
-}
-writeYourGenres();
-function showMyBd() {
-    if (personalMovieBD.privat == false) {
-        console.log(personalMovieBD);
-    }
-}
-showMyBd();
